b608dc538e776634f4114143444bfc1dcfdd768b
José Froes <josepaulo.froes@gmail.com>
2025-11-20 11:14:25 -0300

    Exercicío 01: Crie uma ArrayList de nomes e adicione 5 elementos.

---
+++ src/Ex_01.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex_01 {
+//    Exercicío 01: Crie uma ArrayList de nomes e adicione 5 elementos.
+
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("José");
+        nomes.add("Maria");
+        nomes.add("Carlos");
+        nomes.add("Fernanda");
+        nomes.add("Bruna");
+    }
+}
