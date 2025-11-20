68248641854aea2bb017c402f4b18ee52417e619
José Froes <josepaulo.froes@gmail.com>
2025-11-20 12:15:26 -0300

    Exercicío 09: Crie um HashMap<String, Integer> representando nome → idade.

---
+++ src/Ex_09.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Ex_09 {
+//    Exercicío 09: Crie um HashMap<String, Integer> representando nome → idade.
+
+    public static void main(String[] args) {
+        Map<String, Integer> pessoas = new HashMap<>();
+
+        pessoas.put("Jorge", 29);
+        pessoas.put("Juliane", 30);
+        pessoas.put("Alex", 76);
+        pessoas.put("bob", 59);
+    }
+}
